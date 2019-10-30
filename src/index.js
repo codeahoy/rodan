791d5cac5dc8c5ac0921bee465c9dad93c7adbc4
Umer Mansoor <umermk3@gmail.com>
2019-10-30 08:42:42 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class ApiCall extends React.Component {
+    constructor(){  
+        super();  
+        this.state = {  
+            url: 'http://...',
+            method: 'GET',
+        };  
+   }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+   handleButtonClick = event => {
+       this.setState({url: this.state.title})
+
+   }
+
+   handleUrlChange = event => {
+    this.setState({url: event.target.value});
+  };
+
+  handleHttpMethodChange = event => {
+      this.setState({method: event.target.value});
+  }
+
+    render() {
+        return (
+        <div>
+            <select value={this.state.method} onChange={this.handleHttpMethodChange}>
+            <option value="POST">POST</option>
+            <option value="GET">GET</option>
+            <option value="PUT">PUT</option>
+            </select>
+
+
+            <input
+            type="text"
+            value={this.state.url}
+            onChange={this.handleUrlChange}
+            />
+            <button onClick={this.handleButtonClick}>Call</button>
+
+            <h3> {this.state.method}  {this.state.url} </h3>
+        </div>
+        );
+    }
+};
+
+ 
+ReactDOM.render(<ApiCall />, document.getElementById('root'));
