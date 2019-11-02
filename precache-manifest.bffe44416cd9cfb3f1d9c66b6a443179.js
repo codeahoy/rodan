933359d2c71d455a6d03f49a29888f84548305d7
Umer Mansoor <umermk3@gmail.com>
2019-11-01 19:02:44 -0700

    Updates

---
+++ precache-manifest.bffe44416cd9cfb3f1d9c66b6a443179.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6032167edd5cde37ecc92ada04e5b139",
+    "url": "/umermansoor/rodan/index.html"
+  },
+  {
+    "revision": "baac312cc2aa9b14b790",
+    "url": "/umermansoor/rodan/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "f63a82693311e484439a",
+    "url": "/umermansoor/rodan/static/js/2.9cb7d1ec.chunk.js"
+  },
+  {
+    "revision": "baac312cc2aa9b14b790",
+    "url": "/umermansoor/rodan/static/js/main.7a7df265.chunk.js"
+  },
+  {
+    "revision": "3077e471e9fdfa788ecd",
+    "url": "/umermansoor/rodan/static/js/runtime-main.8e789405.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/umermansoor/rodan/precache-manifest.bffe44416cd9cfb3f1d9c66b6a443179.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/umermansoor/rodan/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
