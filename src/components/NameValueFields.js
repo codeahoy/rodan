3cb016f894003971b43878b41ae0ce74c66e542a
Umer Mansoor <umermk3@gmail.com>
2019-11-01 18:42:13 -0700

    Many changes. See description for more details
    - Added Bootstrap
    - Added support for HTTP headers
    - Moved headers name and value fields to its own component
    NameValueFields so it could be reused for query parameters, etc.
    - Added error handling
    - Added ResultsSection component

---
+++ src/components/NameValueFields.js
@@ -0,0 +1,96 @@
+import React from 'react';
+
+class NameValueFields extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            fields: [],
+        }
+        
+    }
+    addfields = event => {
+        this.setState((prevState) => ({
+            fields: [...prevState.fields, { name: '', value: '' }]
+        })
+        );
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+        if (prevState.fields !== this.state.fields) {
+            this.props.fieldsStateUpdatedCallback(this.state.fields.slice());
+        }
+        
+    }
+
+    btnRemoveFields = event => {
+
+        let indexToRemove = event.target.id;
+
+        if (indexToRemove >= 0) {
+            let newArray = [];
+
+            for (let i = 0; i < this.state.fields.length; i++) {
+                if (i != indexToRemove) {
+                    newArray.push(this.state.fields[i]);
+                }
+            }
+
+            this.setState({fields: newArray.slice()})
+        }
+    }
+
+    inputNameChange = event => {
+        let arr = this.state.fields.slice();
+        let index = event.target.id;
+
+        arr[index].name = event.target.value;
+
+        this.setState({ fields: arr });
+    }
+
+    inputValueChange = event => {
+        let arr = this.state.fields.slice();
+        let index = event.target.id;
+
+        arr[index].value = event.target.value;
+
+        this.setState({ fields: arr });
+    }
+
+    render() {
+
+        return (
+            <div>
+                <h5>{this.props.headingText}</h5>
+                {
+                    this.state.fields.map((val, key) => {
+                        return (
+                            <div className="mt-1" key={key}>
+                                <input
+                                    type="text"
+                                    id={key}
+                                    onChange={this.inputNameChange}
+                                    value={this.state.fields[key].name}
+
+                                />
+
+                                <input
+                                    type="text"
+                                    id={key}
+                                    onChange={this.inputValueChange}
+                                    value={this.state.fields[key].value}
+                                />
+                                <button id={key} onClick={this.btnRemoveFields}>x</button>
+                            </div>
+                        );
+                    })
+                }
+                <button className="mt-1" onClick={this.addfields}>{this.props.buttonText}</button>
+            </div>
+        )
+    }
+}
+
+export default NameValueFields;
--- src/index.js
+++ src/index.js
@@ -1,163 +1,135 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import HeadersForm from './components/HeadersForm';
+import NameValueFields from './components/NameValueFields';
 
-class ApiCall extends React.Component {
-    constructor(){  
-        super();  
-        this.state = {  
+class Main extends React.Component {
+    constructor() {
+        super();
+        this.state = {
             url: 'http://jsonplaceholder.typicode.com/users',
             method: 'GET',
-            headers: []
-        };  
-   }
+            headers: [],
+            errors: [],
+            showResult: false,
+        };
+    }
 
-   handleButtonClick = event => {
-       this.setState({response: null });
+    checkErrors = () => {
 
-       fetch(this.state.url)
-       .then(res =>  { 
-           console.log(res);
-           return res.json() 
-        })
-       .then( (data) => {
-           //console.log(data);
-           this.setState({response: JSON.stringify(data, undefined, 2)});
-       }, reason => {
-           this.setState({response: 'error ' + reason });
-       })
+        let errors = [];
 
-   }
-
-   handleUrlChange = event => {
-    this.setState({url: event.target.value});
-  };
-
-  handleHttpMethodChange = event => {
-      this.setState({method: event.target.value});
-  }
-
-  addHeaders = event => {
-      this.setState( (prevState) => ({
-          headers: [...prevState.headers, {name:'', value: ''}]
-            })
-       );
-  }
-
-  removeHeader = event => {
-
-    let indexToRemove = event.target.id;
-    console.log('del =>' + indexToRemove);
-
-    if (indexToRemove >= 0) {
-        let newArray = [];
-
-        console.log('headers length: '  + this.state.headers.length);
-        for (let i=0; i<this.state.headers.length; i++) {
-            if (i != indexToRemove) {
-                console.log('pushing: ' + this.state.headers[i]);
-                newArray.push(this.state.headers[i]);
-                
+        this.state.headers.forEach((item) => {
+            if (item.name.length === 0) {
+                errors.push('Headers cannot be empty');
             }
+        });
+
+        return errors; 
+    }
+
+    btnSubmitRestCall = event => {
+
+        // This will show the results section
+        this.setState({ showResult: true });
+
+        // Clear previous errors and check for new ones
+        this.setState({ errors: [] });
+
+        let errorsArray = this.checkErrors();
+        if (errorsArray.length > 0) {
+            this.setState({ errors: errorsArray.slice() });
+            return;
         }
 
-        console.log('new array is: ' + newArray)
+        this.setState({ response: 'fetching...' });
 
-        this.setState( {
-            headers: newArray.slice()
-        })
-         
+        fetch(this.state.url, {
+            method: this.state.method,
+            headers: Object.assign({}, ...this.state.headers.map(item => ({ [item.name]: item.value }))),
 
-        //this.setState({headers: newArray});
+        }
+        )
+            .then(res => {
+                console.log(res);
+                return res.json()
+            })
+            .then((data) => {
+                //console.log(data);
+                this.setState({ response: JSON.stringify(data, undefined, 2) });
+            }, reason => {
+                this.setState({ response: 'error ' + reason });
+            })
+    }
 
-        console.log('new headers array is: ' + this.state.headers)
-    } 
-
-  }
-
-  headerNameChange = event => {
-      let arr = this.state.headers.slice();
-      let index = event.target.id;
-
-      arr[index].name = event.target.value;
-
-      this.setState({headers: arr});
-  }
-
-  headerValueChange = event => {
-    let arr = this.state.headers.slice();
-    let index = event.target.id;
-
-    arr[index].value = event.target.value;
-
-    this.setState({headers: arr});
-}
-
-  
-
+    headersStateUpdated = (headersCopy) => {
+        this.setState({ headers: headersCopy })
+    }
 
     render() {
         return (
-        <div>
-            <select value={this.state.method} onChange={this.handleHttpMethodChange}>
-            <option value="POST">POST</option>
-            <option value="GET">GET</option>
-            <option value="PUT">PUT</option>
-            </select>
 
+            <div className="container-fluid mt-3">
+                <div className="row content">
+                    <div className="col-lg-1"></div>
+                    <div className="col-lg-5">
 
-            <input
-            type="text"
-            value={this.state.url}
-            onChange={this.handleUrlChange}
-            />
-            <button onClick={this.handleButtonClick}>Call</button>
-            <br/>
-            <button onClick={this.addHeaders}>Add Header</button>
-            {
-                this.state.headers.map( (val, key) => {
-
-                    let headerNameId = `headerNameId-${key}`, headerValueId = `headerValueId-${key}`;
-
-                    return (
-                        <div>
-                            <input 
+                        <select value={this.state.method} onChange={(e) => this.setState({ method: e.target.value })}>
+                            <option value="POST">POST</option>
+                            <option value="GET">GET</option>
+                            <option value="PUT">PUT</option>
+                        </select>
+                        <input
                             type="text"
-                            name={headerNameId}
-                            data-id={key}
-                            id={key}
-                            onChange={this.headerNameChange}
-                            value={this.state.headers[key].name}
-                            
+                            value={this.state.url}
+                            onChange={(e) => this.setState({ url: e.target.value })}
+                        />
+                        <button onClick={this.btnSubmitRestCall}>Call</button>
+
+                        <br />
+
+                        <NameValueFields headingText='HTTP Header' buttonText='Add Headers' fieldsStateUpdatedCallback={this.headersStateUpdated} />
+
+                    </div>
+                    <div className="col-lg-5">
+                        {/*
+                        Check for errors and render the ResultsSection component with either error message
+                        or the actual response depending on whether there are errors or not.
+                        */}
+
+                        {(this.state.errors.length > 0) ?
+                            <ResultsSection
+                                heading={`Error`}
+                                message={this.state.errors}
                             />
-
-                            <input 
-                            type="text"
-                            name={headerValueId}
-                            data-id={key}
-                            id={key}
-                            onChange={this.headerValueChange}
-                            value={this.state.headers[key].value}
-                 
+                            :
+                            <ResultsSection
+                                heading={`${this.state.method} ${this.state.url}`}
+                                message={this.state.response}
                             />
-                            <button id={key} onClick={this.removeHeader}>x</button>
-                        </div>
+                        }
 
-                        
-                    )
-
-                })
-            }
-            
-            
-
-            <h3> {this.state.method}  {this.state.url} </h3>
-            <pre>{this.state.response}</pre>
-        </div>
+                    </div>
+                    <div className="col-lg-1"></div>
+                </div>
+            </div>
         );
     }
 };
 
- 
-ReactDOM.render(<ApiCall />, document.getElementById('root'));
+class ResultsSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <>
+                <h5> {`Result: ${this.props.heading}`}  </h5>
+                <pre>{this.props.message}</pre>
+            </>
+        );
+    }
+}
+ReactDOM.render(<Main />, document.getElementById('root'));
